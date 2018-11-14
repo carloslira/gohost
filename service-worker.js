f86702c300f694ad4d8e1cb111cf71e1d80c947d
Carlos Lira <carloshenriquen.lira@gmail.com>
2018-11-14 11:25:15 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gohost/precache-manifest.69ec0bae2feb8b65f3e0049be2151045.js"
+  "/gohost/precache-manifest.e19dce1917d395a3544a793d5d98d331.js"
 );
 
 workbox.clientsClaim();
